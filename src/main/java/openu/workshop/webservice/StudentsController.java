9b5d289559d348aa6b5dfeaa330dea47f6742660
Matan Wiesner <matanwiesner@google.com>
2022-12-31 20:24:57 +0200

    return also 401

--- src/main/java/openu/workshop/webservice/StudentsController.java
+++ src/main/java/openu/workshop/webservice/StudentsController.java
@@ -1,10 +1,12 @@
 package openu.workshop.webservice;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class StudentsController {
@@ -19,7 +21,7 @@ public class StudentsController {
 
     String[] loginInfo = new String(Base64.decodeBase64(authParts[1])).split(":");
     if (!loginInfo[0].equals("aaa") || !loginInfo[1].equals("bbb")){
-      throw new RuntimeException();
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
     }
 
 
