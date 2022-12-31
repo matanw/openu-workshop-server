23599b3916cdadd94bb5ca3da616515f2d4cd1ac
Matan Wiesner <matanwiesner@google.com>
2022-12-31 20:23:17 +0200

    basuc auth, no return 401 yet

--- src/main/java/openu/workshop/webservice/StudentsController.java
+++ src/main/java/openu/workshop/webservice/StudentsController.java
@@ -1,14 +1,29 @@
 package openu.workshop.webservice;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentsController {
 
-@GetMapping("/students")
-  public Student GetStudent(@RequestParam int id){
+  @GetMapping("/students")
+  public Student GetStudent(@RequestParam int id,
+      @RequestHeader("Authorization") String authorization){
+    String[] authParts = authorization.split(" ");
+    if (!authParts[0].equals("Basic")){
+      throw new RuntimeException();
+    }
+
+    String[] loginInfo = new String(Base64.decodeBase64(authParts[1])).split(":");
+    if (!loginInfo[0].equals("aaa") || !loginInfo[1].equals("bbb")){
+      throw new RuntimeException();
+    }
+
+
+
     return new Student(id, "matan");
   }
 
