be5cdc75f6bcaa4c296775b016d137bd01ebc517
Matan Wiesner <matanwiesner@google.com>
2023-02-04 20:42:58 +0200

    delete jpa wrapper

--- src/main/java/openu/workshop/webservice/WebserviceApplication.java
+++ src/main/java/openu/workshop/webservice/WebserviceApplication.java
@@ -1,11 +1,5 @@
 package openu.workshop.webservice;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import openu.workshop.webservice.db.JPAWrapper;
-import openu.workshop.webservice.model.Course;
-import openu.workshop.webservice.model.Professor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebserviceApplication {
 
 	//todo: fix "throw Exception" All over code
-	//todo: rethink on JPAWrapper
-
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(WebserviceApplication.class, args);
+	 SpringApplication.run(WebserviceApplication.class, args);
 	}
 }
